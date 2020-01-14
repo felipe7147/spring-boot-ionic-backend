a496c76a58cd9d2a89181b6f775044d86998a0ff
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2020-01-14 10:38:59 -0300

    Listando todas categorias com DTO

--- src/main/java/com/felipealmeida/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/felipealmeida/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -19,7 +19,7 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	@ExceptionHandler(DataIntegrityException.class)
-	public ResponseEntity<StandardError> dataIntegrity(ObjectNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),System.currentTimeMillis() );
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
