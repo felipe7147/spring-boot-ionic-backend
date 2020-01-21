73d066fcb88ccb2f244f1d631daa0e1f21d6f410
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2020-01-21 08:21:00 -0300

    Validacao customizada:email nao repetido na atualizacao de Cliente

--- src/main/java/com/felipealmeida/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/felipealmeida/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.felipealmeida.cursomc.domain.Cliente;
+import com.felipealmeida.cursomc.services.Validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
