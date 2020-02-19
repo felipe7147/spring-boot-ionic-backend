db639dee6de449107310584b5771fe5ffef761ed
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2020-02-19 09:37:47 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/felipealmeida/cursomc/services/CidadeService.java
@@ -0,0 +1,22 @@
+package com.felipealmeida.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipealmeida.cursomc.domain.Cidade;
+import com.felipealmeida.cursomc.repositories.CidadeRepository;
+
+
+
+@Service
+public class CidadeService {
+	
+	@Autowired
+	private CidadeRepository repo;
+
+	public List<Cidade> findByEstado(Integer estadoId) {
+		return repo.findCidades(estadoId);
+	}
+}
---
+++ src/main/java/com/felipealmeida/cursomc/services/EstadoService.java
@@ -0,0 +1,20 @@
+package com.felipealmeida.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipealmeida.cursomc.domain.Estado;
+import com.felipealmeida.cursomc.repositories.EstadoRepository;
+
+@Service
+public class EstadoService {
+	
+	@Autowired
+	private EstadoRepository repo;
+	
+	public List<Estado> findAll() {
+		return repo.findAllByOrderByNome();
+	}
+}
