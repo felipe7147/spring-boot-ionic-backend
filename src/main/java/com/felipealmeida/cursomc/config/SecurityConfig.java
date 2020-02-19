d5c1cf72a8a9d4725395d80014257326cb4c079c
Felipe Almeida <felipe_nunesrodrigues@hotmail.com>
2020-02-19 14:03:49 -0300

    Liberando CORS para PUT e DELETE

--- src/main/java/com/felipealmeida/cursomc/config/SecurityConfig.java
+++ src/main/java/com/felipealmeida/cursomc/config/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.felipealmeida.cursomc.security.JWTAuthenticationFilter;
 import com.felipealmeida.cursomc.security.JWTAuthorizationFilter;
@@ -70,6 +73,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
       
       @Bean
